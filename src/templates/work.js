a85a17c29ab7444740b02a933ed37f578ceda745
Nicola Zanon <nicola.zanon86@gmail.com>
2018-12-20 16:02:19 +0100

    Set up single page

--- src/templates/work.js
+++ src/templates/work.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Layout from '../components/layout'
 import Footer from '../components/footer'
+import styles from './work.module.css'
 
 class WorkTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const { next, prev } = this.props.pageContext
-    console.log(this.props.data, next, prev)
+    const workPrictures = post.frontmatter.pictures.map(({ image }, i) => (
+      <figure key={`pic-${i}`} className={styles.workImage}>
+        <Img fluid={image.childImageSharp.fluid} />
+      </figure>
+    ))
+    console.log(this.props.data)
     return (
       <Layout>
         <section className="section section--page">
           <div className="wrapper">
             <div className="grid">
-              <div className="grid-col col-2-3">sdff</div>
+              <div className="grid-col col-2-3">{workPrictures}</div>
               <div className="grid-col col-1-3">
                 <h1>{post.frontmatter.title}</h1>
                 <h2>{post.frontmatter.subtitle}</h2>
@@ -39,6 +46,15 @@ export const pageQuery = graphql`
         subtitle
         description
         link
+        pictures {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 800, quality: 90) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
       }
     }
   }

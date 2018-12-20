0b81c248580b48cf11a6bb495f2877b5e05f66f4
Nicola Zanon <nicola.zanon86@gmail.com>
2018-12-20 14:54:51 +0100

    Add works page

---
+++ src/pages/gallery/io-cacca.md
@@ -0,0 +1,6 @@
+---
+title: io, cacca
+description: 'oil colours on canvas, 40x80, 2016'
+thumbnail: '/img/hover-1.jpg'
+thumbnailOnHover: '/img/hover-2.jpg'
+---
--- src/pages/works.js
+++ src/pages/works.js
@@ -1,22 +1,41 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-//import Img from 'gatsby-image'
+import { graphql, Link } from 'gatsby'
+import Img from 'gatsby-image'
 
 import styles from './css/works.module.css'
 import Layout from '../components/layout'
 import Footer from '../components/footer'
 
-const WorksPage = ({ data }) =>
-  console.log(data) || (
+const WorksPage = ({ data }) => {
+  console.log(data)
+  const images = data.allMarkdownRemark.edges.map(({ node }, i) => {
+    const { frontmatter, fields } = node
+    return (
+      <div key={`gallery-${i}`} className="grid-col col-1-3">
+        <Link to={fields.slug} className={styles.box}>
+          <div className={styles.boxInner}>
+            <Img fluid={frontmatter.thumbnail.childImageSharp.fluid} />
+          </div>
+          <div className={styles.boxInner}>
+            <Img fluid={frontmatter.thumbnailOnHover.childImageSharp.fluid} />
+          </div>
+        </Link>
+        <h3 className={styles.title}>{frontmatter.title}</h3>
+        <p className={styles.description}>{frontmatter.description}</p>
+      </div>
+    )
+  })
+  return (
     <Layout>
-      <section className="section">
+      <section className="section section--page">
         <div className="wrapper">
-          <div className={styles.grid}>asdd</div>
+          <div className="grid">{images}</div>
         </div>
       </section>
       <Footer />
     </Layout>
   )
+}
 
 export const worksQuery = graphql`
   query PageWorks {

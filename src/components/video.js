aad2e21247112990a2824b155c8699f63f3287bf
Nicola Zanon <nicola.zanon86@gmail.com>
2018-12-21 19:21:35 +0100

    Mobile video

--- src/components/video.js
+++ src/components/video.js
@@ -2,10 +2,54 @@ import React from 'react'
 import { withPrefix } from 'gatsby'
 import styles from './video.module.css'
 
-const Video = () => (
-  <section className={styles.videoWrapper}>
-    <video loop playsInline autoPlay muted src={withPrefix('/video.mp4')} />
-  </section>
-)
+class Video extends React.Component {
+  constructor(props) {
+    super(props)
+    this.video = React.createRef()
+  }
+  state = {
+    isMobile: window.matchMedia('(max-width: 600px)').matches,
+  }
+
+  handleVideo = () => {
+    this.setState({ isMobile: !this.state.isMobile })
+  }
+
+  componentDidMount() {
+    this.media = window.matchMedia('(max-width: 600px)')
+    this.media.addListener(this.handleVideo)
+  }
+
+  componentWillUnmount() {
+    this.media.removeEventListener(this.handleVideo)
+  }
+
+  render() {
+    const { isMobile } = this.state
+    return (
+      <section
+        className={isMobile ? styles.videoWrapperMobile : styles.videoWrapper}
+      >
+        <video
+          ref={this.video}
+          loop
+          playsInline
+          autoPlay
+          muted
+          poster={
+            isMobile
+              ? withPrefix('/poster-mobile.jpg')
+              : withPrefix('/poster-desktop.jpg')
+          }
+          src={
+            isMobile
+              ? withPrefix('/video-mobile.mp4')
+              : withPrefix('/video.mp4')
+          }
+        />
+      </section>
+    )
+  }
+}
 
 export default Video

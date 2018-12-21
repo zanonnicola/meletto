aaaec2910634ac4932d2b449df5f512aecf2cca8
Nicola Zanon <nicola.zanon86@gmail.com>
2018-12-21 15:40:59 +0100

    CSS fixes

--- src/templates/work.js
+++ src/templates/work.js
@@ -13,12 +13,11 @@ function normalizeSlug(slug) {
 }
 class WorkTemplate extends React.Component {
   render() {
-    console.log(this.props)
     const post = this.props.data.markdownRemark
     const imagesToDisplay = post.frontmatter.pictures || []
     const { next, prev } = this.props.pageContext
-    const nextURL = next ? next.fields.slug : '/works'
-    const prevURL = prev ? prev.fields.slug : '/works'
+    const nextURL = Object.keys(next).length ? next.fields.slug : '/works'
+    const prevURL = Object.keys(prev).length ? prev.fields.slug : '/works'
 
     const carousel = this.props.data.carousel.edges
     // Filter the array to rtemove current product
@@ -37,34 +36,39 @@ class WorkTemplate extends React.Component {
         <Img fluid={image.childImageSharp.fluid} />
       </figure>
     ))
+
+    const details = (
+      <React.Fragment>
+        <h1 className={styles.title}>{post.frontmatter.title}</h1>
+        <h2 className={styles.subtitle}>{post.frontmatter.subtitle}</h2>
+        <p className={styles.description}>{post.frontmatter.description}</p>
+        <a className={styles.link} href={post.frontmatter.link}>
+          purchase
+        </a>
+        <div className={styles.arrowsContainer}>
+          <Link to={prevURL}>
+            <img src={sx} alt="prev" />
+          </Link>
+          <Link to={nextURL}>
+            <img src={dx} alt="next" />
+          </Link>
+        </div>
+      </React.Fragment>
+    )
     return (
       <Layout>
         <section className="section section--page">
           <div className="wrapper">
             <div className="grid-f">
               <div className="col-f-2-3">
+                <div className={styles.hideOnDesktop}>{details}</div>
                 {workPrictures}
                 <Link className={styles.backLink} to="/works">
                   &lsaquo; back to all paintings
                 </Link>
               </div>
               <div className="col-f-1-3 sticky">
-                <h1 className={styles.title}>{post.frontmatter.title}</h1>
-                <h2 className={styles.subtitle}>{post.frontmatter.subtitle}</h2>
-                <p className={styles.description}>
-                  {post.frontmatter.description}
-                </p>
-                <a className={styles.link} href={post.frontmatter.link}>
-                  purchase
-                </a>
-                <div className={styles.arrowsContainer}>
-                  <Link to={prevURL}>
-                    <img src={sx} alt="prev" />
-                  </Link>
-                  <Link to={nextURL}>
-                    <img src={dx} alt="next" />
-                  </Link>
-                </div>
+                <div className={styles.hideOnMobile}>{details}</div>
               </div>
             </div>
           </div>

edc955f87bc39c9f7acb83a6b4dfe40c9b3b396b
Nicola Zanon <nicola.zanon86@gmail.com>
2018-12-27 18:38:53 +0100

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,12 +60,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'gatsby-starter-default',
-        short_name: 'starter',
+        short_name: 'Meletto',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#141414',
+        theme_color: '#141414',
         display: 'minimal-ui',
-        icon: 'static/img/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'static/img/favicon-2.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
